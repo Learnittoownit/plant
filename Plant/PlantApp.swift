bbb7bc12c0a16be904f7db2581d51d5c1b8895b3
Rq <iwliluqp@gmail.com>
2025-10-26 05:24:51 -0700

    Initial Commit

---
+++ Plant/PlantApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantApp.swift
+//  Plant
+//
+//  Created by Rama AlQahtani on 25/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct PlantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
